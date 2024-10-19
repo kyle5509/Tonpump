2aeedb0ba6776eade9869bd4fd1043facaa48c48
Eric <kylemcclain540@gmail.com>
2024-10-19 23:05:38 +0100

    manifest url updated

--- tonprovider/TonProvider.tsx
+++ tonprovider/TonProvider.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export function TonProvider({ children }: Props) {
   return (
-    <TonConnectUIProvider manifestUrl="https://tonpump-n822.vercel.app/tonconnect-manifest.json">
+    <TonConnectUIProvider manifestUrl="https://tonpump-five.vercel.app/tonconnect-manifest.json">
       {children}
     </TonConnectUIProvider>
   );
