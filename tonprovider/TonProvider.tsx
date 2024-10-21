423a56f5b348cf690a336240184147e56d1e2f3e
Eric <kylemcclain540@gmail.com>
2024-10-21 23:43:44 +0100

    first commit

--- tonprovider/TonProvider.tsx
+++ tonprovider/TonProvider.tsx
@@ -7,10 +7,8 @@ type Props = {
 
 export function TonProvider({ children }: Props) {
   return (
-    <TonConnectUIProvider manifestUrl="http://localhost:3000/tonconnect-manifest.json">
+    <TonConnectUIProvider manifestUrl="https://tonpump-five.vercel.app/tonconnect-manifest.json">
       {children}
     </TonConnectUIProvider>
   );
 }
-
-// SHA256:5OAYQETnddupmH9l57OU9E5NI43cOxTwvuT2Mc04gU8
